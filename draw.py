f7dd2477da2280081eb28a42586eddc3d3a5ad9a
Knguyen27 <67210962+Yesnt27@users.noreply.github.com>
2024-11-22 15:55:28 -0800

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,5 +1,5 @@
 # TODO: Your header
-
+import random
 import cmpt120image
 
 def recolor_image(img, color):
@@ -9,9 +9,9 @@ def recolor_image(img, color):
   for rows in range(len(img)):
     for cols in range(len(img[0])):
       if img[rows][cols][0] < 240 or img[rows][cols][1] < 240 or img[rows][cols][2] < 240:
-        img2[rows][cols] == color
+        img2[rows][cols] = color
       else:
-        img2[rows][cols] == img[rows][cols]
+        img2[rows][cols] = img[rows][cols]
   return img2
 
 def minify(img):
@@ -19,18 +19,33 @@ def minify(img):
   pass
   
 def mirror(img):
-  # TODO: Add your code here
-  pass
+  img2=cmpt120image.get_black_image(len(img),len(img[0]))
+  for rows in range(len(img)):
+    for cols in range(len(img[0])):
+      if img[rows][cols][0] < 240 or img[rows][cols][1] < 240 or img[rows][cols][2] < 240:
+        img[cols][rows] = img2[cols][(len(img)) - rows - 1]
+
+  return img2
   
 def draw_item(canvas, img, row, col):
-  # TODO: Add your code here
-  pass
+
+  for rows in range(len(img)):
+    for cols in range(len(img[0])):
+      #if img is non-white
+      if img[rows][cols][0] < 240 or img[rows][cols][1] < 240 or img[rows][cols][2] < 240:
+        canvas[rows+row][cols+col] = img[rows][cols]
+  return canvas
   
 def distribute_items(canvas, img, n):
-  # TODO: Add your code here
-  pass
+  for i in range(n):
+    row = random.randint(0,(len(canvas)-len(img)))
+    col = random.randint(1,(len(canvas[0])-len(img[0])))
+    draw_item(canvas,img,row,col)
+  return canvas
+  
 
-apple = cmpt120image.get_image('images/apples.png')
-recolor_image(apple,[12,200,40])
 
-cmpt120image.wait_for_escape()
+# apple = cmpt120image.get_image('images/apples.png')
+# recolor_image(apple,[12,200,40])
+# cmpt120image.show_image(draw_item(apple),"yessir")
+# cmpt120image.wait_for_escape() 
